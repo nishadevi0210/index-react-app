b8a3e9cbba6143508f2bf27e52635de7c844ef06
Nisha Devi <nishadevi0210@gmail.com>
2021-04-24 12:22:15 +0200

    added html and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container pt-3 notepad">
+			<header className="bg-success notes-title">
+				<p className="pp"> Notes
+					<span className="badge badge-light">0</span>
+				</p>
+			</header>
+			<div className="notes-list">
+				<ol className="list-group"></ol>
+				<button type="button" click className="btn btn-success" id="add">Add new note</button>
+			</div>
+			<br />
+			<div className="notes-text">
+				<textarea name="textarea" placeholder="Write here..." id="textArea"></textarea>
+				<button type="button" className="btn btn-success" id="done">Done</button>
+			</div>
+		</div>
+	);
 }
+
 
 export default App;
